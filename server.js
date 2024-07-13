39fa19b353a41fe574349239281fa1f18277ced3
Thakur Neupane <thakur.neupane.neupane@gmail.com>
2024-07-13 20:25:41 +1000

    Sending the html file

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 import express from "express";
-
+import path from "path";
 const app = express();
+
+const __dirname = path.resolve();
+console.log(__dirname, "______");
 
 const PORT = 8001;
 
-app.get("/", (req, res, next) => {
-  console.log("I have got the request");
-  res.send("<h1>Hello Server Dev</h1>;");
-});
+// app.get("/", (req, res, next) => {
+//   console.log("I have got the request");
+//   res.send("<h1>Hello Server Dev</h1>;");
+// });
 
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/src/html/index.html");
+});
 app.listen(PORT, (error) => {
   error
     ? console.log(error)
