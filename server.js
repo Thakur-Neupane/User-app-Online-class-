f2c549e2bbdee5c7757b192c04b07bb7446ac89c
Thakur Neupane <thakur.neupane.neupane@gmail.com>
2024-07-13 20:14:48 +1000

    simple server and responding with the html

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const app = express();
 
 const PORT = 8001;
 
+app.get("/", (req, res, next) => {
+  console.log("I have got the request");
+  res.send("<h1>Hello Server Dev</h1>;");
+});
+
 app.listen(PORT, (error) => {
   error
     ? console.log(error)
