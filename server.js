ef7de3d3318c5a1696e091697f0d67bab6cab945
Thakur Neupane <thakur.neupane.neupane@gmail.com>
2024-07-14 16:18:36 +1000

    User logged in

--- server.js
+++ server.js
@@ -1,71 +1,106 @@
 import express from "express";
+const app = express();
+const PORT = 8000;
 import path from "path";
 import fs from "fs";
-const app = express();
+import { makeHtmlStrig } from "./src/fileMerger.js";
 
 const __dirname = path.resolve();
-console.log(__dirname, "______");
+console.log(__dirname, "======");
 
-const PORT = 8001;
-const fileName = "userList.csv";
+//serve static file from the public directory
+
+app.use(express.static(path.join(__dirname, "public")));
+
 app.use(express.urlencoded({ extended: true }));
 
-// app.get("/", (req, res, next) => {
-//   console.log("I have got the request");
-//   res.send("<h1>Hello Server Dev</h1>;");
+// app.get("/", (req, res) => {
+//   console.log("we got the request");
+
+//   res.send(
+//     "<h1>Hellow server dev</h1> <hr /><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, impedit reiciendis unde ad itaque tenetur officiis quam doloremque vel similique aperiam natus pariatur necessitatibus recusandae quae totam suscipit deserunt nesciunt.</p>"
+//   );
 // });
 
-app.use(express.static(__dirname + "/public"));
-
-// OR we can also do the code like this :-
-
-// path.join(__dirname + "public");
-
-// Homepage controllers
+// hope page controller
 app.get("/", (req, res) => {
-  console.log("request received!!");
-  res.sendFile(__dirname + "/src/html/index.html");
+  //read the text file
+
+  fs.readFile(fileName, "utf8", (error, data) => {
+    if (error) {
+      console.log(error);
+      res.sendFile(__dirname + "/src/html/index.html");
+    } else {
+      res.send(makeHtmlStrig(data.split("\n")));
+    }
+  });
+
+  //   res.sendFile(__dirname + "/src/html/index.html");
 });
 
-app.post("/register", (req, res) => {
-  const { name, email, password } = req.body;
-  const str = `${name}, ${email}, ${password}\n`;
-  fs.appendFile(fileName, str, (error) => {
-    error ? res.send(error.message) : res.redirect("/");
-    // : res.send(
-    //     `<h1 class="alert alert-success">User has been created, You may login now!!!</h1>`
-    //   );
-  });
-  console.log(req.body);
-
-  // create file and write data
-
+// user register controller
+app.get("/register", (req, res) => {
+  console.log(req.query);
+  console.log("req received login");
   res.sendFile(__dirname + "/src/html/register.html");
 });
 
-// app.get("/get-user", (req, res) => {
+const fileName = "userList.csv";
+app.post("/register", (req, res) => {
+  const { name, email, password } = req.body;
+
+  const str = `${name},${email},${password}\n`;
+
+  //   create file and write data
+
+  fs.appendFile(fileName, str, (error) => {
+    error ? res.send(error.message) : res.redirect("/");
+    //   : res.send(
+    //       `<h1 class="alert alert-success">User has been created, you may login now</h1>`
+    //     );
+  });
+
+  //   res.sendFile(__dirname + "/src/html/register.html");
+});
+
+// User Login Controller
+app.get("/login", (req, res) => {
+  console.log("req received login");
+  res.sendFile(__dirname + "/src/html/login.html");
+});
+
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  const str = email + "," + password;
+
+  //read data from file
+
+  fs.readFile(fileName, "utf8", (error, data) => {
+    if (error) {
+      console.log(error);
+      res.send(" ");
+    } else {
+      const person = data.split("\n").find((user) => user.includes(str));
+      person?.length
+        ? res.send(
+            `<h1> Hey ${
+              person.split(",")[0]
+            }, You have successfully logged in</h1>`
+          )
+        : res.send(`<h1> Error, Invalid Login Details </h1>`);
+    }
+  });
+});
+
+// app.get("/api/v1/get-user", (req, res) => {
 //   res.json({
-//     fName: "Thakur",
+//     fName: "Thakur ",
 //     lName: "Neupane",
 //   });
 // });
 
-// User registration controllers
-app.get("/register", (req, res) => {
-  console.log("request received!!");
-  console.log(req.query);
-  res.sendFile(__dirname + "/src/html/register.html");
-});
-
-// User login controllers
-
-app.get("/login", (req, res) => {
-  console.log("data received!!");
-  res.sendFile(__dirname + "/src/html/login.html");
-});
-
 app.listen(PORT, (error) => {
   error
     ? console.log(error)
-    : console.log(` Server is running at http://localhost:${PORT}`);
+    : console.log(`Server is runnit at http://localhost:${PORT}`);
 });
