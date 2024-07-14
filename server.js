cbf4d177fba30e3d463a3a9406223fefc0c7caf5
Thakur Neupane <thakur.neupane.neupane@gmail.com>
2024-07-14 12:11:24 +1000

    login and register

--- server.js
+++ server.js
@@ -18,15 +18,30 @@ app.use(express.static(__dirname + "/public"));
 
 // path.join(__dirname + "public");
 
+// Homepage controllers
 app.get("/", (req, res) => {
+  console.log("request received!!");
   res.sendFile(__dirname + "/src/html/index.html");
 });
 
-app.get("/get-user", (req, res) => {
-  res.json({
-    fName: "Thakur",
-    lName: "Neupane",
-  });
+// app.get("/get-user", (req, res) => {
+//   res.json({
+//     fName: "Thakur",
+//     lName: "Neupane",
+//   });
+// });
+
+// User registration controllers
+app.get("/register", (req, res) => {
+  console.log("request received!!");
+  res.sendFile(__dirname + "/src/html/register.html");
+});
+
+// User login controllers
+
+app.get("/login", (req, res) => {
+  console.log("data received!!");
+  res.sendFile(__dirname + "/src/html/login.html");
 });
 
 app.listen(PORT, (error) => {
